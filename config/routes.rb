fa9b0d24923e377db3383ff06d2f14014cf081d2
Edouard <edouard@tymate.com>
2016-02-03 11:32:41 +0100

    nest url comment into post

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
  
   namespace :api do
     namespace :v1 do 
-      resources :posts, only: [:create, :update, :destroy, :edit, :new, :index, :show] do
-        resources :comments, only: [:create, :update, :destroy, :edit, :new, :index, :show]
+      resources :posts, only: [:create, :update, :destroy, :edit, :index, :show] do
+        resources :comments, only: [:create, :update, :destroy, :edit, :index, :show]
       end
-      resources :users, only: [:create, :update, :destroy, :edit, :new, :index, :show]
+      resources :users, only: [:create, :update, :destroy, :edit, :index, :show]
     end
   end
 end
