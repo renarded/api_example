3a712e4c760bb0f0f2c35f9433976916e37d8520
Edouard <edouard@tymate.com>
2016-02-03 11:51:11 +0100

    add create method for posts

--- app/controllers/api/v1/posts_controller.rb
+++ app/controllers/api/v1/posts_controller.rb
@@ -1,8 +1,14 @@
 class Api::V1::PostsController < ApiController
 
+  skip_before_filter :current_user_logged, only: [:index, :show]
+  before_filter :is_format_type_correct, only: [:create]
+
   def index
-    #posts = Post.where("user_id = :user_id", { user_id: current_user_id })
-    posts = Post.all
+    if permitted_params[:user_id].nil?
+      posts = Post.all 
+    else
+      posts = Post.where("user_id = :user_id", {user_id: permitted_params[:user_id] })
+    end
     render json: posts
   end
 
@@ -11,9 +17,19 @@ class Api::V1::PostsController < ApiController
     render json: post
   end
 
+  def create
+    post = current_user_logged.posts.new(permitted_params)
+
+    if post.save
+      render json: post
+    else
+      render json: { error: "Could not create post." }, status: :unprocessable_entity
+    end
+  end
+
   private
 
   def permitted_params
-    params.permit(:id, :name, :content)
+    params.permit(:id, :name, :content, :user_id)
   end
 end
