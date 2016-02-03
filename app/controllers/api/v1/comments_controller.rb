95f7b9d20b3f6def32d84b5d311b000fc5ce1ec3
Edouard <edouard@tymate.com>
2016-02-03 16:51:24 +0100

    add create method for comments

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -1,5 +1,8 @@
 class Api::V1::CommentsController < ApiController
 
+  skip_before_filter :current_user_logged, only: [:index, :show]
+  before_filter :is_format_type_correct, only: [:create]
+
   def index
     comments = Comment.where("post_id = :post_id",
                  { post_id: permitted_params[:post_id] })
@@ -9,6 +12,17 @@ class Api::V1::CommentsController < ApiController
   def show
     comment = Comment.find(permitted_params[:id])
     render json: comment
+  end
+
+  def create
+    comment = Post.find(permitted_params[:post_id]).comments.new(permitted_params)
+    comment.user_id = current_user_logged.id
+
+    if comment.save
+      render json: comment, status: 201
+    else
+      render json: { error: "Could not create comment." }, status: :unprocessable_entity
+    end
   end
 
   private
